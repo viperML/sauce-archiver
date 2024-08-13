1f3bbf3d9eab20437c47ccd527be955753284c02
Fernando Ayats <ayatsfer@gmail.com>
2024-08-13 21:43:06 +0200

    logging stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,49 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Logger (Loc, LogLevel, LogSource, LogStr, LoggingT (runLoggingT), MonadLogger, defaultOutput, logDebugN, runStdoutLoggingT, fromLogStr)
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Reader
+import System.IO (Handle, stdout)
+import qualified Data.ByteString.Char8 as S8
 
-import Control.Monad.Logger
-import Control.Concurrent.Chan
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever)
+data Config = Config
+    {
+    }
+    deriving (Show)
 
-import qualified Data.Text as T
+newtype App a = App
+    { unApp :: ReaderT Config (LoggingT IO) a
+    }
+    deriving newtype (Functor, Applicative, Monad, MonadIO, MonadLogger, MonadReader Config)
 
-action :: Chan T.Text -> IO ()
-action chan = forever $ do
-    newVal <- readChan chan
-    print newVal
+outputFor ::
+    Loc ->
+    LogSource ->
+    LogLevel ->
+    LogStr ->
+    IO ()
+outputFor loc source level s = S8.putStrLn $ fromLogStr s
+
+runLog :: (MonadIO m) => LoggingT m a -> m a
+runLog action =
+    runLoggingT action outputFor
+
+main' :: App ()
+main' = do
+    logDebugN "Hello"
+    liftIO $ print "Hello"
 
 main :: IO ()
-main = do 
-    -- myChan :: Chan LogLine <- newChan
-    myChan <- newChan
+main = do
+    let config = Config{}
 
-    thread <- forkIO $ action myChan
-
-    print $ T.concat ["[thread:]", (T.pack $ show thread)] 
-
-    _ <- forever $ do
-        line <- getLine
-        writeChan myChan (T.pack line)
+    -- runReaderT (unApp main') config
+    runLog $ runReaderT (unApp main') config
 
     return ()
-
